f6dcacf73bf6e10c3e62e146ce07ecc3c155fac4
Gokup23 <976575334a@gmail.com>
2026-04-08 14:44:39 +0530

    Executed

--- Projects/03_focus_logger/database.py
+++ Projects/03_focus_logger/database.py
@@ -7,6 +7,6 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./focus_logger.db"
 #connectargs required for SQlite only 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 Base = declarative_base()
