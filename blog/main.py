f10acc2d156a0df1851cca1efa2982764a75b33c
Gokup23 <976575334a@gmail.com>
2026-04-18 10:27:03 +0530

    Implement delete functionality for blog posts and enhance show endpoint with error handling

--- blog/main.py
+++ blog/main.py
@@ -23,8 +23,9 @@ def create(request:schemas.Blog ,db : Session = Depends(get_db)):
 
 @app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id,db : Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models)
-
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
 @app.get('/blog')
 def all(db:Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
@@ -32,7 +33,7 @@ def all(db:Session = Depends(get_db)):
 
 @app.get('/blog/{id}',status_code=200)
 def show(id,response : Response , db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id ==id).first() 
+    blog = db.query(models.Blog).filter(models.Blog.id ==id).first()
     if not blog:
         response.status_code = status.HTTP_404_NOT_FOUND
         return { 'detail' : f'blog with the id {id} not found ! '}
