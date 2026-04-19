87da2fef807e5a1e82fcfbf9c59602aa971d5081
Gokup23 <976575334a@gmail.com>
2026-04-19 10:00:36 +0000

    19 Apr 2026, 15:30

--- blog/main.py
+++ blog/main.py
@@ -26,6 +26,17 @@ def destroy(id,db : Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     db.commit()
     return 'done'
+
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = f"Blog with id {id} not found")
+    blog.update({'title': request.title, 'body': request.body}, synchronize_session=False)
+    db.commit()
+    return 'updated'
+
+
 @app.get('/blog')
 def all(db:Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
@@ -37,4 +48,4 @@ def show(id,response : Response , db: Session = Depends(get_db)):
     if not blog:
         response.status_code = status.HTTP_404_NOT_FOUND
         return { 'detail' : f'blog with the id {id} not found ! '}
-    
+    return blog
