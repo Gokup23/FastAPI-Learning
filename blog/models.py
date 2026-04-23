e6712d3122cebb9c9c842e41bb7c14efe53d6dac
Gokup23 <976575334a@gmail.com>
2026-04-23 17:26:51 +0000

    23 Apr 2026, 22:56

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Blog(Base):
     id = Column(Integer , primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
-    user_id = Column(Integer,ForeignKey('users.id'))
+    user_id = Column(Integer,ForeignKey('user.id'))
     creator = relationship("user" , back_populates="blogs")
 
 class user(Base):
