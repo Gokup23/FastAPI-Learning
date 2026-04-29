82c272265edba5b5e08d4458d9a8e1e6d97eea08
Gokup23 <976575334a@gmail.com>
2026-04-29 10:53:13 +0000

    29 Apr 2026, 16:23

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -3,10 +3,10 @@ from typing import List
 from .. import schemas, database, models
 from sqlalchemy.orm import Session 
 
-router = APIRouter(tags=['blogs'])
+router = APIRouter(prefix="/blog" ,tags=['blogs'])
 get_db = database.get_db
 
-@router.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,)
+@router.put('/{id}',status_code=status.HTTP_202_ACCEPTED,)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -15,7 +15,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return 'updated'
 
-@router.get('/blog/{id}',status_code=200,response_model=schemas.ShowBlog,)
+@router.get('/{id}',status_code=200,response_model=schemas.ShowBlog,)
 def show(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id ==id).first()
     if not blog:
@@ -23,7 +23,7 @@ def show(id, db: Session = Depends(get_db)):
         return { 'detail' : f'blog with the id {id} not found ! '}
     return blog
 
-@router.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,)
+@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT,)
 def destroy(id,db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -32,7 +32,7 @@ def destroy(id,db : Session = Depends(get_db)):
     db.commit()
     return 'done'
 
-@router.post('/blog',status_code=status.HTTP_201_CREATED,)
+@router.post('',status_code=status.HTTP_201_CREATED,)
 def create(request:schemas.Blog ,db : Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title , body=request.body, user_id = 1)
     db.add(new_blog)
@@ -40,7 +40,7 @@ def create(request:schemas.Blog ,db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@router.get('/blog', response_model=List[schemas.ShowBlog], )
+@router.get('', response_model=List[schemas.ShowBlog], )
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
