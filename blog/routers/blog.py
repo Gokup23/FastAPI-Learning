12a96bbe381ada08136124c23cec769283b66464
Gokup23 <976575334a@gmail.com>
2026-04-29 10:13:51 +0000

    29 Apr 2026, 15:43

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -25,7 +25,7 @@ def show(id, db: Session = Depends(get_db)):
 
 @router.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=["blogs"])
 def destroy(id,db : Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Damn nothing like that")
     blog.delete(synchronize_session=False)
