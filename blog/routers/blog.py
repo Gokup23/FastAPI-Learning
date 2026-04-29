9ad564b24c3ca327dde3fbcab8a955317e920d26
Gokup23 <976575334a@gmail.com>
2026-04-29 09:34:01 +0000

    29 Apr 2026, 15:04

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter 
+from fastapi import APIRouter , Depends
 from typing import List
 from .. import schemas,database , models
+from sqlalchemy.orm import session
 router = APIRouter()
 
 
 @router.get('/blog',response_model=List[schemas.ShowBlog],tags=["blogs"])
-def all(db:Session = Depends(database.get_db)):
+def all(db:session = Depends(database.get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
