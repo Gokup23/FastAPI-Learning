0488bb16f4d2736708c8f93189a6e5c0faa54db7
Gokup23 <976575334a@gmail.com>
2026-04-29 10:21:51 +0000

    29 Apr 2026, 15:51

--- blog/main.py
+++ blog/main.py
@@ -8,19 +8,5 @@ app = FastAPI()
 models.Base.metadata.create_all(engine)
 
 app.include_router(blog.router)
+app.include_router(user.router)
 
-
-@app.post('/user', response_model=schemas.ShowUser,tags=["users"])
-def user(request: schemas.user, db: Session = Depends(get_db)):
-    hashed_password = hashing.Hash.bcrypt(request.password) 
-    
-    new_user = models.user(
-        name=request.name,
-        email=request.email,
-        password=hashed_password
-    )
-    
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -2,7 +2,23 @@ from fastapi import APIRouter
 from ..import database , schemas ,models
 from fastapi import APIRouter, Depends , status , HTTPException
 from sqlalchemy.orm import session
+from ..hashing import Hash
 
 router = APIRouter
 get_db = database.get_db
- 
+
+@router.post('/user', response_model=schemas.ShowUser,tags=["users"])
+def user(request: schemas.user, db: Session = Depends(get_db)):
+    hashed_password = hashing.Hash.bcrypt(request.password) 
+    
+    new_user = models.user(
+        name=request.name,
+        email=request.email,
+        password=hashed_password
+    )
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
