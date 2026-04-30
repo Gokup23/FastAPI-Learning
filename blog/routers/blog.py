85b61c2f8a99e659e8d5e0c02ef658c67db2b4c1
Gokup23 <976575334a@gmail.com>
2026-04-30 10:31:28 +0000

    30 Apr 2026, 16:01

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -35,13 +35,9 @@ def destroy(id,db : Session = Depends(get_db)):
 
 @router.post('',status_code=status.HTTP_201_CREATED,)
 def create(request:schemas.Blog ,db : Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title , body=request.body, user_id = 1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(db)
 
 @router.get('', response_model=List[schemas.ShowBlog])
 def all(db:Session = Depends(get_db)):
-    return blog.get_all()
+    return blog.get_all(db)
 
