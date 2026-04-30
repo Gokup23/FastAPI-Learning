f818e12eceec71892d4ef0357564c619842c029e
Gokup23 <976575334a@gmail.com>
2026-04-30 10:32:44 +0000

    30 Apr 2026, 16:02

--- blog/repository/blog.py
+++ blog/repository/blog.py
@@ -5,7 +5,7 @@ def get_all(db: Session):
     blogs = db.query(models.Blog).all()
     return blogs
 
-def create():
+def create(request , db:Session ):
     new_blog = models.Blog(title=request.title , body=request.body, user_id = 1)
     db.add(new_blog)
     db.commit()
