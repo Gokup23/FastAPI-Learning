6f3673235e5d7209ef981ab8e03f718870292cf0
Gokup23 <976575334a@gmail.com>
2026-04-30 11:00:43 +0000

    30 Apr 2026, 16:30

--- blog/repository/blog.py
+++ blog/repository/blog.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from .. import models , schemas
+from fastapi import HTTPException , status
 
 def get_all(db: Session):
     blogs = db.query(models.Blog).all()
@@ -11,3 +12,11 @@ def create(request:schemas.Blog, db:Session ):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+def destroy(id:int , db:Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Damn nothing like that")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -26,12 +26,7 @@ def show(id, db: Session = Depends(get_db)):
 
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT,)
 def destroy(id,db : Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Damn nothing like that")
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return 'done'
+    return blog.destroy(id , db)
 
 @router.post('',status_code=status.HTTP_201_CREATED,)
 def create(request:schemas.Blog ,db : Session = Depends(get_db)):
