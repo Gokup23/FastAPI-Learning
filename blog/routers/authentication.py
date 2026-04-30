4f1683ba0abb6e7e2724cda57a85f1587068bccc
Gokup23 <976575334a@gmail.com>
2026-04-30 11:34:12 +0000

    30 Apr 2026, 17:04

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-
+from .. import schemas
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login():
+def login(request:schemas.login):
     return 'login'
