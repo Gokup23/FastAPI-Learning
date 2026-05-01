23c3279d59234e74386b5a076303af68077a860b
Gokup23 <976575334a@gmail.com>
2026-05-01 04:56:18 +0000

    1 May 2026, 10:26

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
-from .. import schemas
+from fastapi import APIRouter , Depends
+from .. import schemas , database , models
+from sqlalchemy.orm import Session
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(request:schemas.login):
+def login(request:schemas.login , db: Session = Depends(database.get_db())):
+    user = db.query(models.user).filter(models.user.email == request.username)
+    if not user:
+        raise 
     return 'login'
