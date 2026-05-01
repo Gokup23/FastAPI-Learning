a2e10d04b2ae251ff0cc28b497cb5c6792378c36
Gokup23 <976575334a@gmail.com>
2026-05-01 05:01:36 +0000

    1 May 2026, 10:31

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -5,7 +5,7 @@ router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
 def login(request:schemas.login , db: Session = Depends(database.get_db)):
-    user = db.query(models.user).filter(models.user.email == request.username)
+    user = db.query(models.user).filter(models.user.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid credentials")
-    return 'login'
+    return user
