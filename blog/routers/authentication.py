2b727b1fd1902072e57334fbe5efa10be221ae00
Gokup23 <976575334a@gmail.com>
2026-05-01 05:00:17 +0000

    1 May 2026, 10:30

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(request:schemas.login , db: Session = Depends(database.get_db())):
+def login(request:schemas.login , db: Session = Depends(database.get_db)):
     user = db.query(models.user).filter(models.user.email == request.username)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid credentials")
